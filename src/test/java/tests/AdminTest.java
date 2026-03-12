30bf5704e3db3abf4d637e9d2f0a60a50e17c2bb
ifako <ivanovma1986@mail.ru>
2026-03-12 15:49:18 +0300

    added myinfo and PIM tests

--- src/test/java/tests/AdminTest.java
+++ src/test/java/tests/AdminTest.java
@@ -1,6 +1,9 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
 import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
 
 public class AdminTest extends BaseTest {
 
@@ -9,6 +12,17 @@ public class AdminTest extends BaseTest {
         app.loginPage.login(app.userCreds.adminLogin, app.userCreds.adminPassword);
         app.adminPage.adminTab.click();
         app.adminPage.usernameField.setValue("admin");
+        app.adminPage.searchButton.click();
+    }
+
+    @Test
+    public void searchAndDropDownCheck() {
+        app.loginPage.login(app.userCreds.adminLogin, app.userCreds.adminPassword);
+        app.adminPage.adminTab.click();
+        app.adminPage.usernameField.setValue("admin");
+        app.adminPage.userRoleButton.click();
+        app.adminPage.userRoleDropDown.getWrappedElement().findElement(byText("Admin")).click();
+        app.adminPage.userRoleButton.shouldHave(Condition.exactText("Admin"));
         app.adminPage.searchButton.click();
     }
 }
